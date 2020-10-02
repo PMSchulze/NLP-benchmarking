6044b7b82109bd5e1692b430cd9a2d7f173120b1
Patrick Schulze <48055572+PMSchulze@users.noreply.github.com>
2020-10-03 00:35:31 +0200

    Update eval_testloss_gpt2.py

--- code/eval_testloss_gpt2.py
+++ code/eval_testloss_gpt2.py
@@ -1,8 +1,10 @@
 import argparse
 
+parser = argparse.ArgumentParser()
 parser.add_argument("--test_corpus")
 parser.add_argument("--model_dir")
 parser.add_argument("--token_vocab")
+args = parser.parse_args()
 
 import torch
 from transformers import GPT2TokenizerFast
