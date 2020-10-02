45f315268131f47764c3da96eddaf4fa0b580c2f
Patrick Schulze <48055572+PMSchulze@users.noreply.github.com>
2020-10-03 00:32:39 +0200

    Update eval_testloss_bert.py

--- code/eval_testloss_bert.py
+++ code/eval_testloss_bert.py
@@ -1,24 +1,32 @@
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--test_corpus")
+parser.add_argument("--model_dir")
+parser.add_argument("--token_vocab")
+args = parser.parse_args()
+
 import torch
-from transformers import BertTokenizerFast
-tokenizer = BertTokenizerFast.from_pretrained('/home/ubuntu/data/token_vocab/bert/')
+from transformers import GPT2TokenizerFast
+tokenizer = GPT2TokenizerFast.from_pretrained(args.token_vocab, additional_special_tokens=['<s>','<pad>','</s>','<unk>','<mask>'], pad_token='<pad>')
 
-from transformers import BertForMaskedLM
-model = BertForMaskedLM.from_pretrained('/home/ubuntu/models/bert/128_10_2_512_10/')
+from transformers import GPT2LMHeadModel
+model = GPT2LMHeadModel.from_pretrained(args.model_dir)
 
-
-with open('/home/ubuntu/data/pretrain_data/wiki_test.txt', encoding="utf-8") as f:
+with open(args.test_corpus, encoding="utf-8") as f:
     lines = [line for line in f.read().splitlines() if (len(line) > 0 and not line.isspace())]
 
 batch_encoding = tokenizer(lines, return_tensors='pt', add_special_tokens=True, truncation=True, max_length=128, padding=True)
 dataset = batch_encoding["input_ids"]
 
 # there is a bug in trainer.evaluate() which produces CUDA OOM error (see https://github.com/huggingface/transformers/issues/7232)
-# therefore we have to split the eval_dataset in pieces 
-dataset_split = torch.split(dataset,200)
+# therefore we have to split the eval_dataset in pieces
+# we split it into pieces of 128 tensors each
+dataset_split = torch.split(dataset,128)
 
 from transformers import DataCollatorForLanguageModeling
 data_collator = DataCollatorForLanguageModeling(
-    tokenizer=tokenizer, mlm=True, mlm_probability=0.15
+    tokenizer=tokenizer, mlm=False,
 )
 
 from transformers import Trainer
@@ -34,3 +42,6 @@ for i in range(0,len(dataset_split)):
     test_loss += trainer.evaluate(dataset_split[i]).get('eval_loss')
 
 test_loss = test_loss/len(dataset_split)
+
+with open(args.model_dir + 'test_loss.txt', "w") as text_file:
+    print("test_loss = {}".format(test_loss), file=text_file)
