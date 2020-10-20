5fbdc63f590b1ae971e50971f6ec6eef9e280061
Patrick Schulze <pa.schulze@campus.lmu.de>
2020-10-20 18:16:59 +0000

    create new folder for glue

---
+++ glue/finetune_single_gpt2.py
@@ -0,0 +1,254 @@
+import argparse
+import numpy as np
+import os
+import pandas as pd
+import random
+from sklearn.metrics import matthews_corrcoef
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader, RandomSampler, SequentialSampler
+from torch.utils.data import TensorDataset
+from transformers import AdamW, get_linear_schedule_with_warmup, GPT2Tokenizer, GPT2Model
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--batch_size", type=int)
+parser.add_argument("--eval_data")
+parser.add_argument("--hidden_size", type=int)
+parser.add_argument("--model_name_or_path")
+parser.add_argument("--num_train_epochs", type=int)
+parser.add_argument("--output_dir")
+parser.add_argument("--seed", type=int)
+parser.add_argument("--task")
+parser.add_argument("--train_data")
+parser.add_argument("--token_vocab")
+args = parser.parse_args()
+
+# Setup CUDA
+if torch.cuda.is_available():
+    device = torch.device("cuda")
+else:
+    device = torch.device("cpu")
+
+# ---------------------------------------------------------------------------------------------------------------
+# First we load & prepare training and evaluation data
+# ---------------------------------------------------------------------------------------------------------------
+
+# Load training & evaluation data into pandas dataframe
+if args.task == 'SST-2':
+    df_train = pd.read_csv(args.train_data, delimiter='\t', header=0, names=['sentence', 'label'])
+    df_eval = pd.read_csv(args.eval_data, delimiter='\t', header=0, names=['sentence', 'label'])
+elif args.task == 'CoLA':
+    df_train = pd.read_csv('/home/ubuntu/data/glue/CoLA/train.tsv', delimiter='\t', header=0, 
+                           names=['abc', 'label', 'xyz', 'sentence'], usecols = ['label', 'sentence'])
+    df_eval = pd.read_csv('/home/ubuntu/data/glue/CoLA/dev.tsv', delimiter='\t', header=0, 
+                          names=['abc', 'label', 'xyz', 'sentence'], usecols = ['label', 'sentence'])
+else:
+    raise Error("Task must be 'SST-2' or 'CoLA'!")
+
+# Store sentences and labels as lists
+sentences_train, sentences_eval = df_train.sentence.to_list(), df_eval.sentence.to_list()
+labels_train, labels_eval = torch.tensor(df_train.label.to_list()), torch.tensor(df_eval.label.to_list())
+
+# Load GPT2 tokenizer
+tokenizer = GPT2Tokenizer.from_pretrained(args.token_vocab, additional_special_tokens=['<s>','<pad>','</s>'], pad_token='<pad>')
+
+# Add <start> and <end> tokens to the tokenizer, as proposed in GPT paper
+tokenizer.add_tokens(["<start>", "<end>"])
+
+# Add <start> and <end> tokens to the input sentences
+sentences_train = ['<start> '+ s + "<end>" for s in sentences_train]
+sentences_eval = ['<start> '+ s + "<end>" for s in sentences_eval]
+
+# Calculate length of the longest sentence
+max_len = 0
+for sent in sentences_train+sentences_eval:
+    ids = tokenizer.encode(sent)
+    max_len = max(max_len, len(ids))
+
+# Tokenize all sentences & store IDs and attention mask
+encoding_train = tokenizer(sentences_train, return_tensors='pt', padding='max_length', truncation=True, max_length = max_len)
+encoding_eval = tokenizer(sentences_eval, return_tensors='pt', padding='max_length', truncation=True, max_length = max_len)
+input_ids_train, input_ids_eval = encoding_train['input_ids'], encoding_eval['input_ids']
+attention_mask_train, attention_mask_eval = encoding_train['attention_mask'], encoding_eval['attention_mask']
+
+# Store IDs, attention masks and labels in one object
+data_train = TensorDataset(input_ids_train, attention_mask_train, labels_train)
+data_eval = TensorDataset(input_ids_eval, attention_mask_eval, labels_eval)
+
+# Set seed before shuffling the batches for reproducability
+random.seed(args.seed)
+np.random.seed(args.seed)
+torch.manual_seed(args.seed)
+torch.cuda.manual_seed_all(args.seed)
+
+# Create input batches
+batches_train = DataLoader(
+    data_train, 
+    sampler = RandomSampler(data_train),
+    batch_size = args.batch_size
+)
+batches_eval = DataLoader(
+    data_eval,
+    sampler = SequentialSampler(data_eval),
+    batch_size = args.batch_size
+)
+
+# ---------------------------------------------------------------------------------------------------------------
+# Here we define the classification head of GPT-2 & initialize the model
+# ---------------------------------------------------------------------------------------------------------------
+
+# We implement the (simple) approach used for the original GPT.
+# That is, the hidden states are fed into a single linear layer to predict the scores.
+class GPT2ForSequenceClassification(nn.Module):
+    def __init__(
+        self,
+        sequence_size: int,
+        n_classes:int ,
+        gpt_model_name_or_path:str,
+    ):
+        super(GPT2ForSequenceClassification,self).__init__()
+        self.gpt2model = GPT2Model.from_pretrained(
+            gpt_model_name_or_path
+        )
+        self.lin = nn.Linear(sequence_size, n_classes)
+
+    def forward(self, ids_in, attention_mask):
+
+        gpt_out = self.gpt2model(ids_in, attention_mask = attention_mask)[0]
+        n_sentences = gpt_out.shape[0]
+        logits = self.lin(gpt_out.view(n_sentences,-1))
+
+        return logits
+
+# Instatiate model
+model = GPT2ForSequenceClassification(
+    sequence_size = max_len * args.hidden_size,
+    n_classes = 2,
+    gpt_model_name_or_path = args.model_name_or_path,
+)
+
+# Add new tokens (<start>, <end>) to the embedding matrix
+# Weights are randomly initialized, as in GPT paper
+model.gpt2model.resize_token_embeddings(len(tokenizer)) 
+
+# Activate CUDA
+model.cuda()
+
+# ---------------------------------------------------------------------------------------------------------------
+# We choose the same optimizer & hyperparameters that we used for BERT and RoBERTa on GLUE
+# ---------------------------------------------------------------------------------------------------------------
+
+# Specify optimizer and hyperparameters
+optimizer = AdamW(
+    model.parameters(),
+    lr = 2e-5,
+    eps = 1e-8
+)
+
+# Calculate number of training steps
+total_steps = len(batches_train) * args.num_train_epochs
+
+# Create the learning rate scheduler (with num_warmup_steps=0, as in other models fine-tuned on GLUE)
+scheduler = get_linear_schedule_with_warmup(
+    optimizer,
+    num_warmup_steps = 0,
+    num_training_steps = total_steps
+)
+
+# ---------------------------------------------------------------------------------------------------------------
+# Train & Eval Loop
+# ---------------------------------------------------------------------------------------------------------------
+
+# Define loss function 
+loss_func = nn.CrossEntropyLoss()
+
+# Initialize empty list to store predictions on evaluation set
+train_eval_hist = []
+logits, true_labels = [], []
+
+# Set seed before training for reproducability
+torch.backends.cudnn.deterministic=True
+random.seed(args.seed)
+np.random.seed(args.seed)
+torch.manual_seed(args.seed)
+torch.cuda.manual_seed_all(args.seed)
+
+# Start training & evaluation loop
+for epoch_e in range(0, args.num_train_epochs):
+    # Training
+    train_loss = 0.0
+    model.train()
+    for step, batch in enumerate(batches_train):
+        input_ids_i = batch[0].to(device)
+        attention_mask_i = batch[1].to(device)
+        true_labels_i = batch[2].to(device)
+        model.zero_grad()
+        preds_i = model(input_ids_i, attention_mask_i)
+        loss = loss_func(preds_i, true_labels_i)
+        train_loss += loss.item()
+        loss.backward()
+        torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+        optimizer.step()
+        scheduler.step()
+    batch_train_loss = train_loss / len(batches_train)
+    # Evaluation
+    model.eval()
+    eval_loss = 0
+    for batch in batches_eval:
+        input_ids_i = batch[0].to(device)
+        attention_mask_i = batch[1].to(device)
+        true_labels_i = batch[2].to(device)
+        with torch.no_grad():
+            preds_i = model(input_ids_i, attention_mask_i)
+            loss = loss_func(preds_i, true_labels_i)
+        eval_loss += loss.item()
+        preds_i = preds_i.detach().cpu()
+        true_labels_i = true_labels_i.to('cpu')
+        logits.append(preds_i)
+        true_labels.append(true_labels_i)
+    batch_eval_loss = eval_loss / len(batches_eval)
+    # Store results of each epoch
+    train_eval_hist.append(
+        {'epoch': epoch_e + 1,
+         'Training Loss': batch_train_loss,
+         'Eval Loss': batch_eval_loss})
+
+
+# Convert logits to predictions and append over all batches
+predictions = np.argmax(np.concatenate(logits, axis=0),axis=1)
+true_labels = np.concatenate(true_labels, axis=0)
+
+# Store evaluation loss
+eval_loss = train_eval_hist[args.num_train_epochs-1].get('Eval Loss')
+
+# ---------------------------------------------------------------------------------------------------------------
+# Save results
+# ---------------------------------------------------------------------------------------------------------------
+
+# Set name of output_dir dependent on task 
+output_dir = args.output_dir + args.task + "/"
+
+# Create output directory if not existing
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+
+# Save model
+torch.save(model.state_dict(), output_dir + 'model')
+
+# Define function to compute accuracy
+def compute_acc(preds, labels):
+    return (preds == labels).mean()
+
+# Save evaluation set results
+if args.task == 'SST-2':
+    eval_acc = compute_acc(predictions, true_labels)
+    with open(output_dir + 'eval_results_sst-2.txt', "w") as text_file:
+        print("eval_loss = {}".format(eval_loss), file=text_file)
+        print("eval_acc = {}".format(eval_acc), file=text_file)
+        print("epoch = {}".format(args.num_train_epochs), file=text_file)
+else:
+    eval_mcc = matthews_corrcoef(predictions,true_labels)
+    with open(output_dir + 'eval_results_sst-2.txt', "w") as text_file:
+        print("eval_loss = {}".format(eval_loss), file=text_file)
+        print("eval_mcc = {}".format(eval_mcc), file=text_file)
+        print("epoch = {}".format(args.num_train_epochs), file=text_file)
---
+++ glue/glue_utils_new.py
@@ -0,0 +1,147 @@
+import torch
+import torch.nn as nn
+from transformers import GPT2Tokenizer, GPT2Model
+
+# Load GPT2 tokenizer
+tokenizer = None
+
+def encode_single(examples):
+    modified_input = ['<|start|>'+ x + '<|end|>' for x in examples['sentence']]
+    return tokenizer(modified_input, truncation = True, padding = 'max_length', max_length = 1024)
+
+def encode_nli(examples, task):
+    part1, part2 = '', ''
+    if task == 'QNLI':
+        part1, part2 = 'question', 'sentence'
+    elif task == 'MNLI':
+        part1, part2 = 'premise', 'hypothesis'
+    else:
+        part1, part2 = 'sentence1', 'sentence2'
+    modified_input = ['<|start|>'+ x + '<$>' + y + '<|end|>' for x,y in zip(examples[part1], examples[part2])]
+    tok = tokenizer(modified_input, truncation = True, padding = 'max_length', max_length = 1024)
+    return tok
+
+def encode_similarity(examples, task):
+    part1, part2 = 'sentence1' if task != 'QQP' else 'question1', 'sentence2' if task != 'QQP' else 'question2'
+    modified_input1 = ['<|start|>'+ x + '<$>' + y + '<|end|>' for x,y in zip(examples[part1], examples[part2])]
+    modified_input2 = ['<|start|>'+ y + '<$>' + x + '<|end|>' for x,y in zip(examples[part1], examples[part2])]
+    tok1 = tokenizer(modified_input1, truncation = True, padding = 'max_length', max_length = 1024)
+    tok2 = tokenizer(modified_input2, truncation = True, padding = 'max_length', max_length = 1024)
+    out = {
+        'attention_mask1': tok1['attention_mask'],
+        'attention_mask2': tok2['attention_mask'],
+        'input_ids1': tok1['input_ids'],
+        'input_ids2': tok2['input_ids'],
+    }
+    return out
+    
+def encode(examples, task):
+    single = {'CoLA', 'SST-2'}
+    nli = {'QNLI', 'RTE', 'WNLI', 'MNLI'}
+    similarity = {'MRPC', 'STS-B', 'QQP'}
+    if task in single:
+        return encode_single(examples)
+    elif task in similarity:
+        return encode_similarity(examples, task)
+    else:
+        return encode_nli(examples, task)
+
+# ---------------------------------------------------------------------------------------------------------------
+# Here we define the classification head of GPT-2 & initialize the model
+# ---------------------------------------------------------------------------------------------------------------
+
+# We implement the approach used for the original GPT.
+#
+# That is, the hidden states which are obtained by the transformer (GPT2Model) 
+# are fed into a single linear layer to predict the scores.
+#
+# For similarity tasks the authors of GPT feed two sequences into separate
+# transformers for each input. The resulting hidden states are then added
+# before fed into the linear layer.
+
+
+# This head takes a single sequence as input.
+# It is used for all GlUE tasks except similarity/paraphrasing. 
+class GPT2ForSequenceClassification(nn.Module):
+    def __init__(
+        self,
+        sequence_size: int,
+        n_classes:int ,
+        gpt_model_name_or_path:str,
+    ):
+        super(GPT2ForSequenceClassification,self).__init__()
+        
+        # Load the pre-trained transformer
+        self.gpt2model = GPT2Model.from_pretrained(
+            gpt_model_name_or_path
+        )
+        # Define a linear layer which predicts scores from hidden states
+        self.lin = nn.Linear(sequence_size, n_classes)
+
+    def forward(self, attention_mask, input_ids):
+        
+        # Calculate hidden states based on pre-trained weights and inputs
+        gpt_out = self.gpt2model(input_ids, attention_mask = attention_mask)[0] 
+        # Extract the hidden states
+        n_sentences = gpt_out.shape[0]
+        # Calculate logits for batch of hidden sequence states
+        logits = self.lin(gpt_out.view(n_sentences,-1))
+        
+        loss = None
+        # Use MSE loss for regression tasks (SST-2) 
+        if n_classes == 1:
+            loss_fct = nn.MSELoss()
+            loss = loss_fct(logits.view(-1), labels.view(-1))
+        # Use cross entropy for classification tasks (all but SST-2)
+        else:
+            loss_fct = nn.CrossEntropyLoss()
+            loss = loss_fct(logits.view(-1, n_classes), labels.view(-1))
+
+        # Return loss and logits for each forward pass
+        return loss, logits
+
+
+# This head takes two sequences as input, each processed by an individual
+# transformers. It is used for all similarity/paraphrasing tasks.
+class GPT2ForSimilarityClassification(nn.Module):
+    def __init__(
+        self,
+        sequence_size: int,
+        n_classes:int ,
+        gpt_model_name_or_path:str,
+    ):
+
+        # Load the pre-trained transformer
+        super(GPT2ForSimilarityClassification,self).__init__()
+        self.gpt2model = GPT2Model.from_pretrained(
+            gpt_model_name_or_path
+        )
+        # Define a linear layer which predicts scores from hidden states
+        self.lin = nn.Linear(sequence_size, n_classes)
+
+    def forward(self, attention_mask1, attention_mask2, input_ids1, input_ids2, labels):
+        
+        # Calculate hidden states for the first sequence
+        gpt_out1 = self.gpt2model(input_ids1, attention_mask = attention_mask1)[0]
+        # Calculate hidden states for the second equence
+        gpt_out2 = self.gpt2model(input_ids2, attention_mask = attention_mask2)[0]
+        # Add the two hidden states element-wise
+        gpt_out = gpt_out1 + gpt_out2
+        # Extract the hidden states
+        n_sentences = gpt_out.shape[0]
+        # Calculate logits for batch of hidden sequence states
+        logits = self.lin(gpt_out.view(n_sentences,-1))
+
+        loss = None
+        # Use MSE loss for regression tasks (SST-2) 
+        if n_classes == 1:
+            loss_fct = nn.MSELoss()
+            loss = loss_fct(logits.view(-1), labels.view(-1))
+        # Use cross entropy for classification tasks (all but SST-2)
+        else:
+            loss_fct = nn.CrossEntropyLoss()
+            loss = loss_fct(logits.view(-1, n_classes), labels.view(-1))
+        
+        # Return loss and logits for each forward pass
+        return loss, logits
+
