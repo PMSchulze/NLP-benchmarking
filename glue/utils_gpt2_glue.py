a3623daafe4db97883639876481cb5598a04d59a
Patrick Schulze <pa.schulze@campus.lmu.de>
2020-10-31 11:49:01 +0000

    truncate to 80 chars

--- glue/utils_gpt2_glue.py
+++ glue/utils_gpt2_glue.py
@@ -12,7 +12,12 @@ tokenizer = None
 
 def encode_single(examples):
     modified_input = ['<|start|>'+ x + '<|end|>' for x in examples['sentence']]
-    return tokenizer(modified_input, truncation = True, padding = 'max_length', max_length = 1024)
+    return tokenizer(
+        modified_input, 
+        truncation = True, 
+        padding = 'max_length',
+        max_length = 1024
+    )
 
 def encode_nli(examples, task):
     part1, part2 = '', ''
@@ -22,16 +27,43 @@ def encode_nli(examples, task):
         part1, part2 = 'premise', 'hypothesis'
     else:
         part1, part2 = 'sentence1', 'sentence2'
-    modified_input = ['<|start|>'+ x + '<$>' + y + '<|end|>' for x,y in zip(examples[part1], examples[part2])]
-    tok = tokenizer(modified_input, truncation = True, padding = 'max_length', max_length = 1024)
+    modified_input = [
+        '<|start|>'+ x + '<$>' + y + '<|end|>' 
+        for x,y in zip(examples[part1], examples[part2])
+    ]
+    tok = tokenizer(
+        modified_input, 
+        truncation = True, 
+        padding = 'max_length',
+        max_length = 1024
+    )
     return tok
 
 def encode_similarity(examples, task):
-    part1, part2 = 'sentence1' if task != 'QQP' else 'question1', 'sentence2' if task != 'QQP' else 'question2'
-    modified_input1 = ['<|start|>'+ x + '<$>' + y + '<|end|>' for x,y in zip(examples[part1], examples[part2])]
-    modified_input2 = ['<|start|>'+ y + '<$>' + x + '<|end|>' for x,y in zip(examples[part1], examples[part2])]
-    tok1 = tokenizer(modified_input1, truncation = True, padding = 'max_length', max_length = 1024)
-    tok2 = tokenizer(modified_input2, truncation = True, padding = 'max_length', max_length = 1024)
+    part1, part2 = (
+        'sentence1' if task != 'QQP' else 'question1', 
+        'sentence2' if task != 'QQP' else 'question2'
+    )
+    modified_input1 = [
+        '<|start|>'+ x + '<$>' + y + '<|end|>' 
+        for x,y in zip(examples[part1], examples[part2])
+    ]
+    modified_input2 = [
+        '<|start|>'+ y + '<$>' + x + '<|end|>' 
+        for x,y in zip(examples[part1], examples[part2])
+    ]
+    tok1 = tokenizer(
+        modified_input1, 
+        truncation = True, 
+        padding ='max_length', 
+        max_length = 1024
+    )
+    tok2 = tokenizer(
+        modified_input2, 
+        truncation = True, 
+        padding = 'max_length', 
+        max_length = 1024
+    )
     out = {
         'attention_mask1': tok1['attention_mask'],
         'attention_mask2': tok2['attention_mask'],
@@ -51,9 +83,9 @@ def encode(examples, task):
     else:
         return encode_nli(examples, task)
 
-# ---------------------------------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 # Here we define the classification head of GPT-2 & initialize the model
-# ---------------------------------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 
 # We implement the approach used for the original GPT.
 #
@@ -175,7 +207,14 @@ class GPT2ForSimilarityClassification(nn.Module):
             m.bias.data.zero_()
             m.weight.data.fill_(1.0)
     
-    def forward(self, attention_mask1, attention_mask2, input_ids1, input_ids2, labels):
+    def forward(
+        self, 
+        attention_mask1, 
+        attention_mask2, 
+        input_ids1, 
+        input_ids2, 
+        labels
+    ):
         
         # Calculate hidden states of all tokens for the first sequence
         gpt_out_all1 = self.gpt2model(
