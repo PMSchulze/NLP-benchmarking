e516b7a2c20deecad61327f11f13145817dd0461
Patrick Schulze <48055572+PMSchulze@users.noreply.github.com>
2020-11-04 03:25:49 +0100

    Update utils_data_preparation.py

--- data_preparation/utils_data_preparation.py
+++ data_preparation/utils_data_preparation.py
@@ -106,7 +106,7 @@ def prepare_nextsentence(input_file, output_file_path):
 # smaller chunks by iteratively adding sentences until the chunk length exceeds 
 # len_short and len_long, respectively. Furthermore, we transfer all short chunks 
 # to short file (these are leftovers from the long cunks and only amount to 3687 
-# chunks out of 699440 total chunks, i.e., about 0.5%). 
+# chunks out of 695393 total chunks, i.e., about 0.5%). 
 # Note that LineByLineTextDataset also produces leftovers (both for long and for short sequences), 
 # so this should not be a big advantage/disadvantage for any model. 
 def divide_into_chunks(
