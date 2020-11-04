73f7769d2fdd3db42054c48ea1f12480b48a131c
Patrick Schulze <pa.schulze@campus.lmu.de>
2020-11-04 16:27:14 +0000

    create new pretraining dir

---
+++ pretraining/language_modeling_utils.py
@@ -0,0 +1,39 @@
+import os
+import pickle
+import torch
+from torch.utils.data.dataset import Dataset 
+from transformers.tokenization_utils import PreTrainedTokenizer
+from typing import Dict, List, Optional
+
+class LineByLineTextDatasetCached(Dataset):
+
+    def __init__(self, tokenizer: PreTrainedTokenizer, file_path: str, block_size: int):
+        assert os.path.isfile(file_path), f"Input file path {file_path} not found"
+
+        directory, filename = os.path.split(file_path)
+        cached_features_file = os.path.join(
+            directory,
+            "cached_lbl_{}_{}_{}".format(
+                tokenizer.__class__.__name__,
+                str(block_size),
+                filename,
+            ),
+        )
+        if os.path.exists(cached_features_file):
+            with open(cached_features_file, "rb") as handle:
+                self.examples = pickle.load(handle)
+        else:
+            with open(file_path, encoding="utf-8") as f:
+                lines = [line for line in f.read().splitlines() if (len(line) > 0 and not line.isspace())]
+
+            batch_encoding = tokenizer(lines, add_special_tokens=True, truncation=True, max_length=block_size)
+            self.examples = batch_encoding["input_ids"]
+            self.examples = [{"input_ids": torch.tensor(e, dtype=torch.long)} for e in self.examples]
+            with open(cached_features_file, "wb") as handle:
+                pickle.dump(self.examples, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+    def __len__(self):
+        return len(self.examples)
+
+    def __getitem__(self, i) -> Dict[str, torch.tensor]:
+        return self.examples[i]
---
+++ pretraining/pretrain_roberta.py
@@ -0,0 +1,102 @@
+import argparse
+from language_modeling_utils import LineByLineTextDatasetCached
+import torch
+from transformers import (
+    DataCollatorForLanguageModeling,
+    RobertaConfig,
+    RobertaForMaskedLM,
+    RobertaTokenizerFast,
+    Trainer,
+    TrainingArguments
+)
+import pickle
+  
+parser = argparse.ArgumentParser()
+parser.add_argument("--hidden_size", type = int)
+parser.add_argument("--num_hidden_layers", type = int)
+parser.add_argument("--num_attention_heads", type = int)
+parser.add_argument("--intermediate_size", type = int)
+parser.add_argument("--num_train_epochs", type = int)
+parser.add_argument("--block_size", type = int)
+parser.add_argument("--batch_size", type = int)
+parser.add_argument("--warmup_steps", type = int)
+parser.add_argument("--corpus_train")
+parser.add_argument("--corpus_eval")
+parser.add_argument("--output_dir")
+parser.add_argument("--token_vocab")
+
+parser.add_argument(
+    "--attention_probs_dropout_prob", type = float, default = 0.1
+)
+parser.add_argument("--hidden_dropout_prob", type = float, default = 0.1) 
+parser.add_argument("--learning_rate", type = float, default = 1e-4) 
+parser.add_argument("--adam_epsilon", type = float, default = 1e-06) 
+parser.add_argument("--adam_beta1", type = float, default = 0.9) 
+parser.add_argument("--adam_beta2", type = float, default = 0.999) 
+parser.add_argument("--weight_decay", type = float, default = 0.01) 
+
+args = parser.parse_args()
+
+
+tokenizer = RobertaTokenizerFast.from_pretrained(
+    args.token_vocab,
+)
+
+data_train = LineByLineTextDatasetCached(
+    tokenizer = tokenizer,
+    file_path = args.corpus_train, 
+    block_size = args.block_size,
+)
+
+data_eval = LineByLineTextDatasetCached(
+    tokenizer = tokenizer,
+    file_path = args.corpus_eval, 
+    block_size = args.block_size,
+)
+
+config = RobertaConfig(
+    vocab_size = len(tokenizer),
+    hidden_size = args.hidden_size,
+    num_hidden_layers = args.num_hidden_layers,
+    num_attention_heads = args.num_attention_heads,
+    intermediate_size = args.intermediate_size,
+    attention_probs_dropout_prob = args.attention_probs_dropout_prob,
+    hidden_dropout_prob = args.hidden_dropout_prob,
+)
+
+model = RobertaForMaskedLM(config = config)
+
+data_collator = DataCollatorForLanguageModeling(
+    tokenizer = tokenizer, 
+    mlm = True, 
+    mlm_probability = 0.15
+)
+
+training_args = TrainingArguments(
+    output_dir = args.output_dir, 
+    overwrite_output_dir = True,
+    learning_rate = args.learning_rate,
+    adam_epsilon = args.adam_epsilon,
+    adam_beta1 = args.adam_beta1,
+    adam_beta2 = args.adam_beta2,
+    weight_decay = args.weight_decay,
+    warmup_steps = args.warmup_steps,
+    num_train_epochs = args.num_train_epochs,
+    per_device_train_batch_size = args.batch_size,
+    save_steps = 10_000,
+    save_total_limit = 1,
+    do_eval = True,
+    evaluation_strategy = 'epoch',
+)
+trainer = Trainer(
+    model = model,
+    args = training_args,
+    data_collator = data_collator,
+    train_dataset = data_train,
+    eval_dataset = data_eval,
+    # prediction_loss_only = True,
+)
+
+trainer.train()
+
+trainer.save_model(args.output_dir)
